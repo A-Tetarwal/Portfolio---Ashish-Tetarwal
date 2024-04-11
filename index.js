f996f63b02f6da43fb5158d5cfd55e34af3a5c82
A-Tetarwal <andashishwill@gmail.com>
2024-04-11 14:27:56 +0530

    added portfolio

---
+++ index.js
@@ -0,0 +1,17 @@
+document.getElementById('proceedButton').addEventListener('click', function() {
+    var navDiv = document.getElementById('aboutMe');
+    // Scroll to the nav div
+    navDiv.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('homeButton').addEventListener('click', function() {
+    var navDiv = document.getElementById('page1');
+    // Scroll to the nav div
+    navDiv.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('aboutBtn').addEventListener('click', function() {
+    var navDiv = document.getElementById('aboutMe');
+    // Scroll to the nav div
+    navDiv.scrollIntoView({ behavior: 'smooth' });
+});
